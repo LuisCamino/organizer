8f1a86acff2190e81977932cb6cbf84f3ceaff0a
LuisCamino <luiscm4c@gmail.com>
2021-07-12 21:39:21 +0200

    optimized text block

--- file_organizer.py
+++ file_organizer.py
@@ -3,10 +3,13 @@ import shutil
 
 
 # bloque texto
+txt_array = [".txt", ".doc", ".docx", "odt", "wri"]
 if os.path.isdir('text'):
     for txt_file in os.listdir("./"):
-        if txt_file.endswith(".txt") or txt_file.endswith(".doc"):
-            shutil.move(txt_file, "./text")
+        for tex in txt_array:
+            if txt_file.endswith(tex):
+                shutil.move(txt_file, "./text")
+
 else:
     os.mkdir("text")
     for txt_file in os.listdir("./"):
@@ -15,6 +18,7 @@ else:
 
 
 # bloque imagenes
+img_array = [".jpg", ".jpeg", ".png"]
 if os.path.isdir('img'):
     for img_file in os.listdir("./"):
         if img_file.endswith(".jpg") or img_file.endswith(".png") or img_file.endswith(".jpeg"):
