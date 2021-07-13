6f4b669afd3a79b8051454ef0a261fc9e11350fd
LuisCamino <luiscm4c@gmail.com>
2021-07-13 21:43:49 +0200

    completed img and pdf with new features

--- file_organizer.py
+++ file_organizer.py
@@ -3,32 +3,53 @@ import shutil
 
 
 # bloque texto
-txt_array = [".txt", ".doc", ".docx", "odt", "wri"]
+txt_array = [".txt", ".doc", ".docx", ".odt", ".wri"]
 for txt_file in os.listdir("./"):
     for tex in txt_array:
-        fila = txt_file
-        filo = tex
+        current_text = txt_file
+        target_text = tex
 
         if os.path.isdir('text'):
-            if txt_file.endswith(filo):
-                shutil.move(fila, "./text")
+            if txt_file.endswith(target_text):
+                shutil.move(current_text, "./text")
 
         else:
-
-            if txt_file.endswith(filo):
-                if fila:
+            if txt_file.endswith(target_text):
+                if current_text:
                     os.mkdir("text")
-                    shutil.move(fila, "./text")
+                    shutil.move(current_text, "./text")
 
 
 # bloque imagenes
-img_array = [".jpg", ".jpeg", ".png"]
-if os.path.isdir('img'):
-    for img_file in os.listdir("./"):
-        if img_file.endswith(".jpg") or img_file.endswith(".png") or img_file.endswith(".jpeg"):
-            shutil.move(img_file, "./img")
-else:
-    os.mkdir("img")
-    for img_file in os.listdir("./"):
-        if img_file.endswith(".jpg") or img_file.endswith(".png") or img_file.endswith(".jpeg"):
-            shutil.move(img_file, "./img")
+img_array = [".jpg", ".png", ".jpeg", ".tif", ".tiff", ".svg"]
+for img_file in os.listdir("./"):
+    for im in img_array:
+        current_img = img_file
+        target_img = im
+
+        if os.path.isdir('img'):
+            if img_file.endswith(target_img):
+                shutil.move(current_img, "./img")
+
+        else:
+            if img_file.endswith(target_img):
+                if current_img:
+                    os.mkdir("img")
+                    shutil.move(current_img, "./img")
+
+# bloque pdf
+pdf_array = [".pdf", ".PDF"]
+for pdf_file in os.listdir("./"):
+    for pf in pdf_array:
+        current_pdf = pdf_file
+        target_pdf = pf
+
+        if os.path.isdir('pdf'):
+            if pdf_file.endswith(target_pdf):
+                shutil.move(current_pdf, "./pdf")
+
+        else:
+            if pdf_file.endswith(target_pdf):
+                if current_pdf:
+                    os.mkdir("pdf")
+                    shutil.move(current_pdf, "./pdf")
